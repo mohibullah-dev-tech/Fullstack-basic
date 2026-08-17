8f0e5c9ef9aed72fca5ef8b91e6d1d1c2cd72610
Md Mohibullah <mdmohibullah352@gmail.com>
2026-08-17 20:23:20 +0600

    added first server ready

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-const express = require('express');
+import express from 'express'
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.send('server is ready')
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {    
+  console.log('Server is running on port 3000')
+});
+
