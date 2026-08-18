23fc8901a0bc27d9df9a8b53ffdd4b2818bd4601
Md Mohibullah <mdmohibullah352@gmail.com>
2026-08-18 11:47:49 +0600

    get jokesfrom backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
 import "./App.css";
+import axios from "axios";
 
 function App() {
   const [jokes, setJokes] = useState([]);
 
+  useEffect(() => {
+    axios
+      .get("/api/jokes")
+      .then((res) => {
+        setJokes(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [jokes]);
+
   return (
     <>
       <h1>Hello World</h1>
+
       <p>JOKES: {jokes.length}</p>
 
       {jokes.map((joke) => (
